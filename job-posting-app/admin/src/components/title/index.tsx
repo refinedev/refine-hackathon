22ee2831388b8387e626c070d2d1d7ed4980a154
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2022-02-18 16:21:30 +0300

    Migration refine 1->2.

--- job-posting-app/admin/src/components/title/index.tsx
+++ job-posting-app/admin/src/components/title/index.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Link } from "@pankod/refine";
 
 import { TitleProps } from "@pankod/refine";
 
 export const Title: React.FC<TitleProps> = ({ collapsed }) => (
-  <Link to="/">
+  <>
     {collapsed ? (
       <img
         src={"/refine-collapsed.svg"}
@@ -26,5 +25,5 @@ export const Title: React.FC<TitleProps> = ({ collapsed }) => (
         }}
       />
     )}
-  </Link>
+  </>
 );
